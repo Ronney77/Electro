910c0d24a6322333eb381985f32cb56893c11af8
Ronney77 <isgenderovriyad9@gmail.com>
2023-04-20 16:08:51 +0400

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    screens:{
-      
-    },
     extend: {},
   },
   plugins: [],
 }
+
